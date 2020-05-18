75566827ef9bdaf84b2b4c9966c423e6a1aa12bd
Tracy Kobel <tracy@tracykobel.name>
2020-05-17 23:52:15 -0500

    Add ApiController Attirbute

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -5,6 +5,7 @@ using StarChart.Data;
 namespace StarChart.Controllers
 {
     [Route("")]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
